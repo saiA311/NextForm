dca80b08243731ed88f570cbfaa617ede7d5c291
saisantosh p <saisantosh.ssp@gmail.com>
2021-01-13 19:21:15 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -4,4 +4,5 @@ module.exports = {
       "mongodb+srv://admin:rDFpHdecVMkwKIi2@cluster0.fxnwp.mongodb.net/formdb?retryWrites=true&w=majority",
   },
   distDir: "build",
+  target: "serverless",
 };
