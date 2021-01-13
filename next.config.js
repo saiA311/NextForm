450728900b26a4ab350d49e1adf69e315e2548ee
saisantosh p <saisantosh.ssp@gmail.com>
2021-01-13 20:42:40 +0530

    recommit

--- next.config.js
+++ next.config.js
@@ -4,5 +4,4 @@ module.exports = {
       "mongodb+srv://admin:rDFpHdecVMkwKIi2@cluster0.fxnwp.mongodb.net/formdb?retryWrites=true&w=majority",
   },
   distDir: "build",
-  target: "serverless",
 };
